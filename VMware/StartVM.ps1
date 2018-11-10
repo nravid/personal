69f6eec50c1110f4240bb8daff6eb70bc7810640
Nate Ravid <nravid@gothamtg.net>
2018-11-10 11:14:27 -0500

    Update for new lab

--- VMware/StartVM.ps1
+++ VMware/StartVM.ps1
@@ -1,36 +1,26 @@
-Import-Module C:\labbuildr2017\vmxtoolkit\vmxtoolkit
 
-$nsxmgr = get-vmx | where {$_.VMXName -eq "lab-nsxmgr-01"} | Get-VMXInfo
-$labvc = get-vmx | where {$_.VMXName -eq "lab-vc-01"} | Get-VMXInfo
-$labdc = get-vmx | where {$_.VMXName -eq "lab-dc-01"} | Get-VMXInfo
-$labvpx = get-vmx | where {$_.VMXName -eq "lab-vpx-01"} | Get-VMXInfo
-$freenas = get-vmx | where {$_.VMXName -eq "FreeNAS"} | Get-VMXInfo
-$vyos = get-vmx | where {$_.VMXName -eq "VYOS"} | Get-VMXInfo
-$esx1 = get-vmx | where {$_.VMXName -eq "ESX-01"} | Get-VMXInfo
-$esx2 = get-vmx | where {$_.VMXName -eq "ESX-02"} | Get-VMXInfo
-$esx3 = get-vmx | where {$_.VMXName -eq "ESX-03"} | Get-VMXInfo
 
-Start-VMX -VMXName "lab-vc-01"
+#Start-VMX -VMXName "lab-vc-01"
+#Start-VMX -VMXName "FreeNAS"
+#Start-VMX -VMXName "lab-dc-01"
+#Start-VMX -VMXName "lab-nsxmgr-01"
+#Start-VMX -VMXName "ESX-02"
+#Start-VMX -VMXName "ESX-03"
+
 Start-VMX -VMXName "VYOS"
-Start-VMX -VMXName "FreeNAS"
-Start-VMX -VMXName "lab-dc-01"
-Start-VMX -VMXName "lab-nsxmgr-01"
-Start-VMX -VMXName "lab-vpx-01"
-Start-VMX -VMXName "ESX-01"
-Start-VMX -VMXName "ESX-02"
-Start-VMX -VMXName "ESX-03"
+Start-VMX -VMXName "VPX"
+Start-VMX -VMXName "NMAS"
 
-Start-Sleep -s 540
+#Start-Sleep -s 540
 
-Import-Module VMware.PowerCLI
-Connect-VIServer -Server lab-vc-01.anoprop.com
+#Connect-VIServer -Server lab-vc-01.anoprop.com
 
 
-Start-VM -VM lab-nsxesg-01-0 -RunASync
-Start-VM -VM lab-nsxdlr-01-0 -RunASync
-Start-VM -VM lab-viewconn72-01 -RunASync
-Get-VMQuestion | Set-VMQuestion -Option "button.yes" -Confirm:$false
-Start-VM -VM lab-uag30-01 -RunASync
+#Start-VM -VM lab-nsxesg-01-0 -RunASync
+#Start-VM -VM lab-nsxdlr-01-0 -RunASync
+#Start-VM -VM lab-viewconn72-01 -RunASync
+#Get-VMQuestion | Set-VMQuestion -Option "button.yes" -Confirm:$false
+#Start-VM -VM lab-uag30-01 -RunASync
 # Start-VM -VM vrni-platform -RunASync
 # Start-VM -VM vrni-proxy -RunASync
 # Start-VM -VM vRops -RunASync
--- VMware/StopVM.ps1
+++ VMware/StopVM.ps1
@@ -1,28 +1,30 @@
-Import-Module VMware.PowerCLI
-Connect-VIServer -Server lab-vc-01.anoprop.com
+#Connect-VIServer -Server lab-vc-01.anoprop.com
 
-Get-VM | Shutdown-VMGuest -Confirm:$false
+#Get-VM | Shutdown-VMGuest -Confirm:$false
 
-Start-Sleep -s 120
+#Start-Sleep -s 120
 
-Stop-VMHost -VMHost esx-01.anoprop.com -Force -Confirm:$false -RunASync
-Stop-VMHost -VMHost esx-02.anoprop.com -Force -Confirm:$false -RunASync
-Stop-VMHost -VMHost esx-03.anoprop.com -Force -Confirm:$false -RunASync
+#Stop-VMHost -VMHost esx-01.anoprop.com -Force -Confirm:$false -RunASync
+#Stop-VMHost -VMHost esx-02.anoprop.com -Force -Confirm:$false -RunASync
+#Stop-VMHost -VMHost esx-03.anoprop.com -Force -Confirm:$false -RunASync
 
-Start-Sleep -s 360
+#Start-Sleep -s 360
 
-Import-Module C:\labbuildr2017\vmxtoolkit\vmxtoolkit
 
-$nsxmgr = get-vmx | where {$_.VMXName -eq "lab-nsxmgr-01"} | Get-VMXInfo
-$labvc = get-vmx | where {$_.VMXName -eq "lab-vc-01"} | Get-VMXInfo
-$labdc = get-vmx | where {$_.VMXName -eq "lab-dc-01"} | Get-VMXInfo
-$labvpx = get-vmx | where {$_.VMXName -eq "lab-vpx-01"} | Get-VMXInfo
-$freenas = get-vmx | where {$_.VMXName -eq "FreeNAS"} | Get-VMXInfo
-$vyos = get-vmx | where {$_.VMXName -eq "VYOS"} | Get-VMXInfo
+#$nsxmgr = get-vmx | where {$_.VMXName -eq "lab-nsxmgr-01"}
+#$labvc = get-vmx | where {$_.VMXName -eq "lab-vc-01"}
+#$labdc = get-vmx | where {$_.VMXName -eq "lab-dc-01"}
+#$labvpx = get-vmx | where {$_.VMXName -eq "lab-vpx-01"}
+#$freenas = get-vmx | where {$_.VMXName -eq "FreeNAS"}
+$nmas = get-vmx | where {$_.VMXName -eq "nmas"}
+$vpx = get-vmx | where {$_.VMXName -eq "VPX"}
+$vyos = get-vmx | where {$_.VMXName -eq "vyos"}
 
-Stop-VMX -VMXName $nsxmgr.VMXName -Mode soft -config $nsxmgr.Configfile
-Stop-VMX -VMXName $labvc.VMXName -Mode soft -config $labvc.Configfile
-Stop-VMX -VMXName $labdc.VMXName -Mode soft -config $labdc.Configfile
-Stop-VMX -VMXName $labvpx.VMXName -Mode soft -config $labvpx.Configfile
-Stop-VMX -VMXName $freenas.VMXName -Mode soft -config $freenas.Configfile
-Stop-VMX -VMXName $vyos.VMXName -Mode soft -config $vyos.Configfile
+#Stop-VMX -VMXName $nsxmgr.VMXName -Mode soft -config $nsxmgr.Config
+#Stop-VMX -VMXName $labvc.VMXName -Mode soft -config $labvc.Config
+#Stop-VMX -VMXName $labdc.VMXName -Mode soft -config $labdc.Config
+#Stop-VMX -VMXName $labvpx.VMXName -Mode soft -config $labvpx.Config
+#Stop-VMX -VMXName $freenas.VMXName -Mode soft -config $freenas.Config
+Stop-VMX -VMXName $nmas.VMXName -Mode soft -config $nmas.Config
+Stop-VMX -VMXName $vpx.VMXName -Mode soft -config $vpx.Config
+Stop-VMX -VMXName $vyos.VMXName -Mode soft -config $vyos.Config

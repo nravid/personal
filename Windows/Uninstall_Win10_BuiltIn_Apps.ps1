931e2f0c5d15e255482234216b05ff0aaa1e65a3
Nate Ravid <nravid@gothamtg.net>
2018-11-10 10:43:15 -0500

    Update Windows/Uninstall_Win10_BuiltIn_Apps.ps1

---
+++ Windows/Uninstall_Win10_BuiltIn_Apps.ps1
@@ -0,0 +1,41 @@
+﻿<#
+Based on 
+http://www.digitalcitizen.life/how-remove-default-windows-10-apps-powershell-3-steps
+
+Run as Administrator
+#>
+
+
+$applist = ("3dbuilder",
+"alarms",
+"calculator",
+"communications",
+"camera",
+"dolbyaccess",
+"fitbitcoach",
+"officehub",
+"skypeapp",
+"getstarted",
+"zunemusic",
+"maps",
+"solitairecollection",
+"bingfinance",
+"zunevideo",
+"bingnews",
+"onenote",
+"people",
+"windowsphone",
+"phototastic",
+"photos",
+"picsart",
+"windowsstore",
+"bingsports",
+"soundrecorder",
+"bingweather",
+"xbox")
+
+ForEach ($app in $applist) {
+    $appdel = $null
+    $appdel = "*" + $app + "*"
+    Get-AppxPackage $appdel | Remove-AppxPackage
+} #ForEach $app
